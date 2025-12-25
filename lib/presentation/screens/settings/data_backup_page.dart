a2d7f506c8222b41b5e5a95f9e613fb2b5ba97dd
azrim <mirzaspc@gmail.com>
2025-12-25 13:34:28 +0700

    feat: force restart app after importing backup

--- lib/presentation/screens/settings/data_backup_page.dart
+++ lib/presentation/screens/settings/data_backup_page.dart
@@ -7,6 +7,7 @@ import 'package:boorusphere/presentation/provider/data_backup/entity/backup_opti
 import 'package:boorusphere/presentation/provider/data_backup/entity/backup_result.dart';
 import 'package:boorusphere/presentation/utils/extensions/buildcontext.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
